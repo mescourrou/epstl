e2223ba1501f21817d19cbbbda78dcb53f00f912
Maxime Escourrou <maxime@escourrou.ovh>
2020-03-30 22:01:14 +0200

    feat: modulo

--- include/math.hpp
+++ include/math.hpp
@@ -1,9 +1,25 @@
 #pragma once
 
 #include <type_traits>
+#include <exception.hpp>
 
 namespace epstl
 {
+class value_exception : public exception
+{
+  public:
+    explicit value_exception() noexcept = default;
+    explicit value_exception(const char* what) noexcept :
+        exception(what) {}
+};
+
+
+template <typename T>
+T abs(T nb)
+{
+    return nb > 0 ? nb : -nb;
+}
+
 
 /**
  * @brief Get the max of the two elements
@@ -39,6 +55,51 @@ template < typename T, typename ...Args>
 T max(T a, T b, Args... others)
 {
     return max(a, max(b, others...));
+}
+
+template<typename T>
+T modulo(T nb, T mod)
+{
+    bool negative = false;
+    mod = abs(mod);
+    if (nb < 0)
+    {
+        nb = -nb;
+        negative = true;
+    }
+
+    while (nb > mod)
+    {
+        nb -= mod;
+    }
+
+    if (negative)
+        return -nb;
+    else
+        return nb;
+}
+
+template<typename T>
+T modulo2(T nb, T modLow, T modHigh)
+{
+    if (modLow >= modHigh)
+        throw epstl::value_exception("Lower bound of the modulo2 needs to be lower than the higher bound");
+    if (nb < modLow)
+    {
+        while (nb < modLow)
+        {
+            nb += modHigh - modLow;
+        }
+        return nb;
+    }
+    else
+    {
+        while (nb >= modHigh)
+        {
+            nb -= modHigh - modLow;
+        }
+        return nb;
+    }
 }
 
 } // namespace epstl
--- unit_tests/mathTest.cpp
+++ unit_tests/mathTest.cpp
@@ -9,4 +9,14 @@ TEST_F(mathTest, Max)
     EXPECT_EQ(max(1, 2, 3, 4, 5), 5);
 }
 
+TEST_F(mathTest, Modulo)
+{
+    EXPECT_TRUE(abs(modulo(1.2, 0.5) - 0.2) < 0.0001);
+    EXPECT_TRUE(abs(modulo(-1.2f, 1.f) - (-0.2)) < 0.0001);
+
+    EXPECT_TRUE(abs(modulo2(1.2, -1., 2.) - 1.2) < 0.0001);
+    EXPECT_TRUE(abs(modulo2(2.2, -1., 2.) + 0.8) < 0.0001);
+    EXPECT_TRUE(abs(modulo2(0.2, 1., 3.) - 2.2) < 0.0001);
+}
+
 } // namespace epstl
